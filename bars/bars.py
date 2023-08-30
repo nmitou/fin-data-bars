d16755262efc08dde68e1a673b106b1696d76b8c
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-08-30 18:29:24 +0200

    docstrings

--- bars/bars.py
+++ bars/bars.py
@@ -10,41 +10,117 @@ class BarsBase:
 	def __init__(self, threshold, file_path, **kwargs):
 		"""
 	    Construct BarsBase object, initialised with threshold (dependent on bar (sub-)type), file path to CSV or text document along with other keyword
-        arguments to be passed to pandas read_csv function to construct a pandas dataframe.
+        arguments to be passed to the pandas read_csv function to construct a pandas dataframe.
+        Required keyword arguments include index_col specifying the column number to be used as the dataframe's index which should be datetimes, these
+        are parsed automatically so there is no need to specify the parse_dates argument. Additionally, a column with the name < price > must be specified
+        as well as a < volume > column if creating volume bars.
+        BarsBase was not intended to be constructed as an instance, rather only to be used as a base for it sub-classes.
 
 	    Parameters
 	    ----------
-	    threshold : int, time
-	        DESCRIPTION.
+	    threshold : int
+	        threshold value for corresponding (sub-)bar type..
 	    file_path : str
 	        Path to data file/csv.
 	    **kwargs 
 	        Keyword arguments to pass through to pandas.read_csv.
+            See above for required keyword arguments.
+            
+	    Returns
+	    -------
+	    None.
+        
 	    """
 		self._threshold = threshold
 		self.set_tick_data(file_path, **kwargs)
+		self._bars_data = None
 
 	def get_threshold(self):
+		"""
+	    Getter method for threshold value.
+
+	    Returns
+	    -------
+	    int
+	        threshold value for corresponding (sub-)bar type.
+
+	    """
 		return self._threshold
 
 	def set_threshold(self, threshold):
+		"""
+	    Setter method for threshold value.
+
+	    Parameters
+	    ----------
+	    threshold : int
+	        threshold value for corresponding (sub-)bar type.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		self._threshold = threshold
 
 	def get_tick_data(self):
+		"""
+	    Getter method for extracting dataframe of tick data inputted from csv file.
+
+	    Returns
+	    -------
+	    pandas.DataFrame
+	        pandas DataFrame of tick data with corresponding columns.
+
+	    """
 		return self._tick_data
 
 	def set_tick_data(self, file_path, **kwargs):
+		"""
+	    Setter method for setting the tick_data pandas DataFrame with a csv file along with other keyword arguments to be passed to the pandas read_csv function.
+        Required keyword arguments include index_col specifying the column number to be used as the dataframe's index which should be datetimes, these
+        are parsed automatically so there is no need to specify the parse_dates argument. Additionally, a column with the name < price > must be specified
+        as well as a < volume > column if creating volume bars.
+
+	    Parameters
+	    ----------
+	    file_path : str
+	        Path to data file/csv.
+	    **kwargs 
+	        Keyword arguments to pass through to pandas.read_csv.
+            See above for required keyword arguments.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		if 'parse_dates' in kwargs:
 			del kwargs['parse_dates']
 		self._tick_data = pd.read_csv(filepath_or_buffer = file_path, parse_dates = True, **kwargs)
+
+	def get_bars_data(self):
+		"""
+	    Getter method for extracting the corresponding bars data constructed from the tick data.
+
+	    Returns
+	    -------
+	    pandas.DataFrame
+	        pandas DataFrame of bars where each row constitutes a bar with columns Open, High, Low and Close.
+
+	    """
+		return self._bars_data
 
 
 class TickBars(BarsBase):
 
 	def __init__(self, threshold, file_path, **kwargs):
 		"""
-	    Construct TickBars object where trade bars are group by number (threshold) of ticks/trades.
-        Trade data will be read in from the specified file path and other keyword arguments will be passed to pandas.read_csv to construct a pandas dataframe.
+	    Construct TickBars object where trade bars are grouped by number of ticks/trades.
+        TickBars objects are initialised with a threshold specifying the number of ticks per bar, file path to CSV or text document along with other keyword
+        arguments to be passed to the pandas read_csv function to construct a pandas dataframe.
+        Required keyword arguments include index_col specifying the column number to be used as the dataframe's index which should be datetimes, these
+        are parsed automatically so there is no need to specify the parse_dates argument. Additionally, a column with the name < price > must be specified.
 
 	    Parameters
 	    ----------
@@ -54,10 +130,25 @@ class TickBars(BarsBase):
             Path to data file/csv.
         **kwargs 
             Keyword arguments to pass through to pandas.read_csv.
+            See above for required keyword arguments.
+        
+        Returns
+	    -------
+	    None.
+        
 	    """
 		super().__init__(threshold, file_path, **kwargs)
 
 	def make_bars(self):
+		"""
+	    
+
+	    Returns
+	    -------
+	    TYPE
+	        DESCRIPTION.
+
+	    """
 		if self.get_threshold() == 0:
 			return pd.DataFrame(columns=['Open', 'High', 'Low', 'Close'])
 		data = []
@@ -87,14 +178,55 @@ class TickBars(BarsBase):
 class TimeBars(BarsBase):
 
 	def __init__(self, threshold, file_path, **kwargs):
+		"""
+	    
+
+	    Parameters
+	    ----------
+	    threshold : TYPE
+	        DESCRIPTION.
+	    file_path : TYPE
+	        DESCRIPTION.
+	    **kwargs : TYPE
+	        DESCRIPTION.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		super().__init__(threshold, file_path, **kwargs)
 		self._unit = 'minutes' # default value for units, rather set in setter method than constructor to abide by Liskov substitution principle
 		self.set_threshold_Timedelta(threshold, self._unit)
 
 	def set_unit(self, unit):
+		"""
+	    
+
+	    Parameters
+	    ----------
+	    unit : TYPE
+	        DESCRIPTION.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		self.set_threshold(self._threshold, unit)
 
 	def get_threshold(self):
+		"""
+	    
+
+	    Returns
+	    -------
+	    TYPE
+	        DESCRIPTION.
+	    TYPE
+	        DESCRIPTION.
+
+	    """
 		return (self._threshold, self._unit)
 
 	def set_threshold(self, threshold, unit):
@@ -104,14 +236,47 @@ class TimeBars(BarsBase):
 		self.set_threshold_Timedelta(threshold, unit)
 
 	def get_threshold_Timedelta(self):
+		"""
+	    
+
+	    Returns
+	    -------
+	    TYPE
+	        DESCRIPTION.
+
+	    """
 		return self._dt
 
 	def set_threshold_Timedelta(self, threshold, unit):
+		"""
+	    
+
+	    Parameters
+	    ----------
+	    threshold : TYPE
+	        DESCRIPTION.
+	    unit : TYPE
+	        DESCRIPTION.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		self._threshold = threshold
 		self._unit = unit
 		self._dt = pd.Timedelta(self._threshold, self._unit)
 
 	def make_bars(self):
+		"""
+	    
+
+	    Returns
+	    -------
+	    TYPE
+	        DESCRIPTION.
+
+	    """
 		if self.get_threshold()[0] == 0:
 			return pd.DataFrame(columns=['Open', 'High', 'Low', 'Close'])
 		data = []
@@ -146,9 +311,35 @@ class TimeBars(BarsBase):
 class VolumeBars(BarsBase):
 
 	def __init__(self, threshold, file_path, **kwargs):
+		"""
+	    
+
+	    Parameters
+	    ----------
+	    threshold : TYPE
+	        DESCRIPTION.
+	    file_path : TYPE
+	        DESCRIPTION.
+	    **kwargs : TYPE
+	        DESCRIPTION.
+
+	    Returns
+	    -------
+	    None.
+
+	    """
 		super().__init__(threshold, file_path, **kwargs)
 
 	def make_bars(self):
+		"""
+	    
+
+	    Returns
+	    -------
+	    TYPE
+	        DESCRIPTION.
+
+	    """
 		if self.get_threshold() == 0:
 			return pd.DataFrame(columns=['Open', 'High', 'Low', 'Close'])
 		data = []
