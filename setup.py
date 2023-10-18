2200805554f9b54892bf89d00f77d7e097596dff
Nic Mitoulis <120728902+nmitou@users.noreply.github.com>
2023-10-18 17:22:45 +0200

    fix indentation

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
-setup(	name = 'bars',
-		version = '0.1',
-		description = 'Package used to calculate different trade bars/candle sticks based on tick data.',
-		author = 'Nicolas Mitoulis',
-		packages = ['bars'],
-		zip_safe = False)
+setup(name = 'bars',
+      version = '0.1',
+      description = 'Package used to calculate different trade bars/candle sticks based on tick data.',
+      author = 'Nicolas Mitoulis',
+      packages = ['bars'],
+      zip_safe = False)
